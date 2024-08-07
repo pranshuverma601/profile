faacf4f9059e3baaa0adc3473ce685d29d3b0252
adesh1145 <adesh1145>
2024-08-07 22:35:05 +0530

    commit by adesh

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    base: '/pranshuverma601.github.io/', // Update this to your repository name
+    plugins: [react()],
+});
