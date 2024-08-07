4356a26f9a9f32a4b09b07e312a6829089cc7dc7
adesh1145 <adesh1145>
2024-08-07 22:54:25 +0530

    /profile on vit.config.js fil

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/pranshuverma601.github.io/', // Update this to your repository name
+    base: '/profile/', // Adjust this to your GitHub Pages repo name if necessary
     plugins: [react()],
 });
